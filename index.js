575f2795104d0dda774f78f9b6bd134afd48532b
MadlenGeorgieva <madlengeorgieva2005@gmail.com>
2025-12-14 19:48:33 +0100

    v2

---
+++ index.js
@@ -0,0 +1,55 @@
+const galleryData = [
+    {
+        img: "Pictures/Create Aarhus Home.jpg",
+        title: "Aarhus",
+        subtitle: "Create"
+    },
+    {
+        img: "Pictures/Domen Home.jpg",
+        title: "Domen's Coffee",
+        subtitle: ""
+    },
+    {
+        img: "Pictures/Creating Moments Home.jpg",
+        title: "Moments",
+        subtitle: "Creating"
+    }
+];
+
+let currentIndex = 1; // start with center image
+
+const leftImg = document.querySelector(".image-left img");
+const centerImg = document.querySelector(".image-central img");
+const rightImg = document.querySelector(".image-right img");
+
+const leftText = document.querySelector(".image-left h3");
+const centerText = document.querySelector(".image-central h3");
+const rightText = document.querySelector(".image-right h3");
+
+const leftBtn = document.querySelector(".arrow-left");
+const rightBtn = document.querySelector(".arrow-right");
+
+function updateGallery() {
+    const leftIndex = (currentIndex - 1 + galleryData.length) % galleryData.length;
+    const rightIndex = (currentIndex + 1) % galleryData.length;
+
+    leftImg.src = galleryData[leftIndex].img;
+    centerImg.src = galleryData[currentIndex].img;
+    rightImg.src = galleryData[rightIndex].img;
+
+    leftText.textContent = galleryData[leftIndex].title;
+    centerText.textContent = galleryData[currentIndex].title;
+    rightText.textContent = galleryData[rightIndex].title;
+}
+
+updateGallery();
+
+rightBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % galleryData.length;
+    updateGallery();
+});
+
+leftBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + galleryData.length) % galleryData.length;
+    updateGallery();
+});
